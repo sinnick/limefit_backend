c7f5cc3106838722395eaabbd68c91472f800908
Fernando Masso <fercsbmwlp@gmail.com>
2026-02-03 22:49:18 +0000

    config: add basePath /limefit for nginx proxy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:19006/api/:path*',
-      },
-    ]
-  }
+  basePath: '/limefit',
 }
 
 module.exports = nextConfig
